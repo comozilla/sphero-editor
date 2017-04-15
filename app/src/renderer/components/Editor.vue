6c87161c1440e9a8be05f432cd98b84e9e7d8bc4
nakamurataichi <ringo.m3@icloud.com>
2017-04-15 16:20:25 +0900

    supported editor

--- app/src/renderer/components/Editor.vue
+++ app/src/renderer/components/Editor.vue
@@ -1,39 +1,50 @@
-<template>
-  <div id="editor">
-    <textarea v-model="motion" placeholder="コードを入力してください" id="textarea"></textarea>
-    <button @click="submitMotion">送信</button>
+<template>    
+  <div id="wrapper">
+    <div id="editor"></div>
   </div>
 </template>
 
 <script>
 import publisher from "@renderer/publisher";
+import ace from "brace";
+import "brace/mode/javascript";
+import "brace/theme/xcode";
 
 export default {
   data() {
     return {
-      motion: ""
+      editor: null,
+      isSetupEditor: false,
     };
+  },
+  mounted() {
+    this.editor = ace.edit("editor");
+    this.editor.setTheme("ace/theme/xcode");
+    this.editor.setShowInvisibles(true);
+    this.editor.setShowPrintMargin(false);
+    const session = this.editor.getSession();
+    session.setMode("ace/mode/javascript");
+    session.setTabSize(2);
+    session.setUseSoftTabs(true);
+    this.isSetupEditor = true;
   },
   methods: {
     submitMotion() {
-      publisher.publish("parse", this.motion);
+      publisher.publish("parse", this.editor.getValue());
     }
   }
 };
 </script>
 
 <style scoped>
-#editor {
+#wrapper {
   display: block;
   width: 100%;
-  height: 100%;
+  flex: 1;
   background-color: #34495e;
 }
-#textarea {
+#editor {
   width: 100%;
-  height: calc(100% - 30px);
-  resize: none;
-  background-color: #34495e;
-  box-sizing: border-box;
+  height: 100%;
 }
 </style>
