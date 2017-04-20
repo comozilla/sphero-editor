f662de98622221a19ed2c755fce2b6dbe9a3a0ce
shundroid <shundroidk@gmail.com>
2017-04-20 17:01:36 +0900

    fixed eslint

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,24 +1,23 @@
-'use strict'
+"use strict";
 
-process.env.BABEL_ENV = 'renderer'
+process.env.BABEL_ENV = "renderer";
 
-const path = require('path')
-const pkg = require('./app/package.json')
-const settings = require('./config.js')
-const webpack = require('webpack')
+const path = require("path");
+const pkg = require("./app/package.json");
+const webpack = require("webpack");
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const ignoreExternals = [
   "brace"
 ];
 
 let rendererConfig = {
-  devtool: '#eval-source-map',
+  devtool: "#eval-source-map",
   devServer: { overlay: true },
   entry: {
-    renderer: path.join(__dirname, 'app/src/renderer/main.js')
+    renderer: path.join(__dirname, "app/src/renderer/main.js")
   },
   externals: Object.keys(pkg.dependencies || {}).filter(p => ignoreExternals.indexOf(p) === -1),
   module: {
@@ -26,36 +25,36 @@ let rendererConfig = {
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
+          fallback: "style-loader",
+          use: "css-loader"
         })
       },
       {
         test: /\.html$/,
-        use: 'vue-html-loader'
+        use: "vue-html-loader"
       },
       {
         test: /\.js$/,
-        use: 'babel-loader',
-        include: [ path.resolve(__dirname, 'app/src/renderer') ],
+        use: "babel-loader",
+        include: [ path.resolve(__dirname, "app/src/renderer") ],
         exclude: /node_modules/
       },
       {
         test: /\.json$/,
-        use: 'json-loader'
+        use: "json-loader"
       },
       {
         test: /\.node$/,
-        use: 'node-loader'
+        use: "node-loader"
       },
       {
         test: /\.vue$/,
         use: {
-          loader: 'vue-loader',
+          loader: "vue-loader",
           options: {
             loaders: {
-              sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax=1',
-              scss: 'vue-style-loader!css-loader!sass-loader'
+              sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax=1",
+              scss: "vue-style-loader!css-loader!sass-loader"
             }
           }
         }
@@ -63,65 +62,65 @@ let rendererConfig = {
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         use: {
-          loader: 'url-loader',
+          loader: "url-loader",
           query: {
             limit: 10000,
-            name: 'imgs/[name].[ext]'
+            name: "imgs/[name].[ext]"
           }
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         use: {
-          loader: 'url-loader',
+          loader: "url-loader",
           query: {
             limit: 10000,
-            name: 'fonts/[name].[ext]'
+            name: "fonts/[name].[ext]"
           }
         }
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('styles.css'),
+    new ExtractTextPlugin("styles.css"),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './app/index.ejs',
-      appModules: process.env.NODE_ENV !== 'production'
-        ? path.resolve(__dirname, 'app/node_modules')
+      filename: "index.html",
+      template: "./app/index.ejs",
+      appModules: process.env.NODE_ENV !== "production"
+        ? path.resolve(__dirname, "app/node_modules")
         : false,
     }),
     new webpack.NoEmitOnErrorsPlugin()
   ],
   output: {
-    filename: '[name].js',
-    libraryTarget: 'commonjs2',
-    path: path.join(__dirname, 'app/dist')
+    filename: "[name].js",
+    libraryTarget: "commonjs2",
+    path: path.join(__dirname, "app/dist")
   },
   resolve: {
     alias: {
-      '@components': path.join(__dirname, 'app/src/renderer/components'),
-      '@renderer': path.join(__dirname, 'app/src/renderer'),
-      'w3c-blob': path.join(__dirname, 'alias/blob.js')
+      "@components": path.join(__dirname, "app/src/renderer/components"),
+      "@renderer": path.join(__dirname, "app/src/renderer"),
+      "w3c-blob": path.join(__dirname, "alias/blob.js")
     },
-    extensions: ['.js', '.vue', '.json', '.css', '.node'],
+    extensions: [".js", ".vue", ".json", ".css", ".node"],
     modules: [
-      path.join(__dirname, 'app/node_modules'),
-      path.join(__dirname, 'node_modules')
+      path.join(__dirname, "app/node_modules"),
+      path.join(__dirname, "node_modules")
     ]
   },
-  target: 'electron-renderer'
-}
+  target: "electron-renderer"
+};
 
 /**
  * Adjust rendererConfig for production settings
  */
-if (process.env.NODE_ENV === 'production') {
-  rendererConfig.devtool = ''
+if (process.env.NODE_ENV === "production") {
+  rendererConfig.devtool = "";
 
   rendererConfig.plugins.push(
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
+      "process.env.NODE_ENV": "\"production\""
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
@@ -131,7 +130,7 @@ if (process.env.NODE_ENV === 'production') {
         warnings: false
       }
     })
-  )
+  );
 }
 
-module.exports = rendererConfig
+module.exports = rendererConfig;
